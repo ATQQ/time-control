af7a33d38d2b586c7d2715a9b53f1e88e9488ee8
ATQQ <engineerzjl@foxmail.com>
2020-12-25 09:41:46 +0800

    chore: 更新一波测试用例

--- test/json/index.js
+++ test/json/index.js
@@ -4,7 +4,7 @@ const path = require('path')
 
 const testData = fs.readFileSync(path.relative(__dirname, 'test.md'), { encoding: 'utf-8' })
 
-const { getJSON } = require('./../src/utils')
+const { getJSON } = require('../../src/utils')
 
 
 fs.writeFileSync(path.resolve(__dirname, 'test.json'), JSON.stringify(getJSON(testData)), { encoding: 'utf-8' })
