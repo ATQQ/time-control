87154d7b64ffbce492a63df705eac5c022a02122
ATQQ <engineerzjl@foxmail.com>
2021-08-18 23:37:03 +0800

    feat: 移除无关配置代码

--- src/page/main.js
+++ src/page/main.js
@@ -1,11 +1,9 @@
 import { createApp } from 'vue';
 import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
-import locale from 'element-plus/lib/locale/lang/zh-cn';
 import App from './App.vue';
 
-// 将自动设置 Day.js 的国际化设置为 'zh-cn'
 const app = createApp(App);
-app.use(ElementPlus, { locale });
+app.use(ElementPlus);
 
 app.mount('#app');
